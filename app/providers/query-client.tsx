94027bb4d2de6ecd389363f2123515b81ed05b14
datnguyen-boompay <dat@boompay.app>
2025-02-17 03:32:42 +0700

    login ui

---
+++ app/providers/query-client.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export function QueryClientProviderWrapper({
+                                             children
+                                           }: {
+  children: ReactNode;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
